a80e609386dd6e36a532cea85f0d2bfc090ebbd2
Eldon <Birdheldon@gmail.com>
2025-06-20 22:09:20 -0600

    Dataframe solution?
    I made some file changes. Code, I basically just added an array for the strucuture so taht I can pass it into my code. This might need changes later.

--- src/cmake-build-debug/CMakeFiles/Bio_dpt.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Bio_dpt.
-# This may be replaced when dependencies are built.
--- src/pcr_lib.cpp
+++ src/pcr_lib.cpp
@@ -1,7 +1,9 @@
 ﻿#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 #include <stdio.h>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 namespace py = pybind11;
 
@@ -12,15 +14,34 @@ class snp_data {
         std::string primer_sequence;
         std::string direction;
         int length;
+		std::string gc_content;
+		std::string hairpin;
+		std::string homodimer;
+
 
         std::string get(const std::string& key) const {
    			if (key == "rsid") return rsid;
     		if (key == "allele") return allele;
     		if (key == "primer_sequence") return primer_sequence;
     		if (key == "direction") return direction;
+    		if (key == "gc_content") return gc_content;
+    		if (key == "hairpin") return hairpin;
+    		if (key == "homodimer") return homodimer;
     		if (key == "length") return std::to_string(length);
+            throw std::invalid_argument("Invalid key name");
 		}
 };
+
+class snp_array{
+	  public:
+			std::vector<snp_data> data;
+
+			snp_data get(const int index) const {
+                          if( index < 0){ throw std::out_of_range("the index is out of raeng"); }
+
+	                  return data[index];
+			}
+  };
 
 
 
@@ -62,7 +83,15 @@ PYBIND11_MODULE(pcr_lib, m) {
         .def_readwrite("primer_sequence", &snp_data::primer_sequence)
         .def_readwrite("direction", &snp_data::direction)
         .def_readwrite("length", &snp_data::length)
+        .def_readwrite("gc_content", &snp_data::gc_content)
+        .def_readwrite("hairpin", &snp_data::hairpin)
+        .def_readwrite("homodimer", &snp_data::homodimer)
     	.def("__getitem__", &snp_data::get);
+
+    py::class_<snp_array>(m, "snp_array")
+		.def(py::init<>())
+    	.def_readwrite("data", &snp_array::data)
+    	.def("__getitem__", &snp_array::get);
 
 
 }
