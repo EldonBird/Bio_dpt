d4f478834d9c7b5dc0b20d1396b18ce934ed069d
Eldon <Birdheldon@gmail.com>
2025-07-11 23:33:23 -0600

    Fixes & Generating Allele Specific Primers
    I have changed around a few things internally as I have learned that you can simply return vectors and python will know that it should be a list inside of python!!! So there is a bit of moving stuff around. I have also gotten the Generate Allele Specific Primers function ~completed~. My only notes are listed inside the code if you search for "check" or something like that you are sure to find them.

--- src/pcr_lib.cpp
+++ src/pcr_lib.cpp
@@ -1,91 +1,61 @@
-﻿#include <stdexcept>
+﻿#include <algorithm>
+#include <ranges>
+#include <stdexcept>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <stdio.h>
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <bits/fs_ops.h>
 #include <pstl/execution_defs.h>
 
 namespace py = pybind11;
 
 class primer {
     public:
+		// ?
         std::string rsid;
+		std::string snp_id;
+
+		std::string allele;
+		std::string primer_sequence;
+		std::string direction;
+
+		float score;
+		float gc;
+		float tm;
+		float hairpin;
+		float homodimer;
 		int position;
-        std::string allele;
-        std::string primer_sequence;
-        std::string direction;
         int length;
-		std::string gc_content;
-        int tm;
-		int hairpin;
-		int homodimer;
-
-
-        std::string get(const std::string& key) const {
-   			if (key == "rsid") return rsid;
-    		if (key == "allele") return allele;
-        	if (key == "position") return std::to_string(position);
-    		if (key == "primer_sequence") return primer_sequence;
-    		if (key == "direction") return direction;
-    		if (key == "gc_content") return gc_content;
-    		if (key == "tm") return std::to_string(tm);
-    		if (key == "hairpin") return std::to_string(hairpin);
-    		if (key == "homodimer") return std::to_string(homodimer);
-    		if (key == "length") return std::to_string(length);
-            throw std::invalid_argument("Invalid key name");
-		}
 };
 
-class primer_group{
-	  public:
-			std::vector<primer> data;
-
-			primer get(const int index) const {
-                          if( index < 0){ throw std::out_of_range("the index is out of range"); }
-
-	                  return data[index];
-			}
-			void add(primer p) {
-				data.push_back(p);
-			}
-};
-
-primer_group generate_allele_spesific_primers(primer_group Snp_data, int min_length, int max_length) {
-	primer_group result;
-
-	for (int i = 0; i < Snp_data.data.size(); i++) {
-
-		int snp_id = Snp_data.get(i).snp_id;
-		std::string allele = Snp_data.get(i).allele;
-		std::string sequence = Snp_data.get(i).primer_sequence;
-		int center = Snp_data.get(i).position;
-
-		for (int i = min_length; i < max_length + 1; i++) {
-	
-			std::string foward = sequence.substr(center, sequence.length());
-			std::string foward_missmatches = introduce_mismatch(foward, i);
-
-			
-			for
-
-
-			
-		}for length in range(min_len, max_len + 1):
-            # Forward prime
-		
-
-
-
-		
+std::string reverse_complement(std::string s) {
+  
+	std::unordered_map<char, char> complement = {
+      
+		{'A', 'T'}, {'T', 'A'},
+		{'C', 'G'}, {'G', 'C'},
+		{'a', 't'}, {'t', 'a'},
+		{'c', 'g'}, {'g', 'c'}
+    
+	};
+  
+	std::string result;
+    
+	for(int i = 0; i < s.length(); i++) {
+      
+		char cur_complement = complement[s[i]];
+		result += cur_complement;
+      
 	}
-
-	
+    
+    
 	return result;
 }
 
-std::string introduce_missmatch(std::string str) {
+std::string introduce_mismatch(std::string str) {
 
 	std::vector<std::string> result;
 
@@ -124,6 +94,76 @@ std::string introduce_missmatch(std::string str) {
 	return new_str;	
 }
 
+std::vector<primer> generate_allele_specific_primers(std::vector<primer> data, int min_length, int max_length) {
+	std::vector<primer> result;
+
+	for (int i = 0; i < data.data.size(); i++) {
+
+		std::string snp_id = data[i].snp_id;
+		std::string allele = data[i].allele;
+		std::string sequence = data[i].primer_sequence;
+		int center = data[i].position;
+
+		std::string forward = sequence.substr(center - max_length + 1, center + 1);
+		std::string forward_mismatch = introduce_mismatch(forward);
+
+		if (forward.length() >= min_length) {
+
+			for (int length = 0; length < max_length - min_length - 1; length++) {
+
+				std::string trimmed = forward_mismatch.substr(length);
+				primer p;
+
+				// might have a constructor later, please keep this in consideration
+
+				p.snp_id = snp_id;
+				p.allele = allele;
+				p.primer_sequence = trimmed;
+				p.direction = "forward";
+				p.length = forward.length() - length;
+
+				result.push_back(p);
+			}
+		}
+		else {
+			throw std::invalid_argument("Error: thte forward length was not greater than or equal to the min length for item, " +
+				std::to_string(i) + " : " + snp_id + " . This error happened in the generate_allele_specific_primers");
+		}
+
+
+		// I have no idea if this is what was intended, please have someone double check this ;)
+		std::string reverse = reverse_complement(sequence.substr(center + 1, center + max_length - 1));
+		std::string reverse_mismatch = introduce_mismatch(reverse);
+
+		if (reverse.length() >= min_length) {
+
+			for (int length = 0; length < max_length - min_length - 1; length++) {
+
+				// double check that this is doing what I think that it is doing
+				std::string trimmed = reverse_mismatch.substr(length);
+				primer p;
+
+				p.snp_id = snp_id;
+				p.allele = allele;
+				p.primer_sequence = trimmed;
+				p.direction = "reverse";
+				p.length = reverse.length() - length;
+
+				result.push_back(p);
+			}
+		}
+		else {
+			throw std::invalid_argument("Error: the reverse length was not greater than or equal to the min length for item, " +
+				std::to_string(i) + " : " + snp_id + " . This error happened in the generate_allele_specific_primers");
+		}
+	}
+
+	
+	return result;
+}
+
+
+
 std::unordered_map<std::string, std::string> evaluate_primer(const std::string seq) {
 
 	std::unordered_map<std::string, std::string> result;
@@ -134,11 +174,11 @@ std::unordered_map<std::string, std::string> evaluate_primer(const std::string s
 	return result;
 }
 
-primer_group filter_primers(primer_group evaluated_primers, int tm_min, int tm_max, double hairpin_max, double homodimer_max) {
-  	primer_group result;
+std::vector<primer> filter_primers(std::vector<primer> evaluated_primers, int tm_min, int tm_max, double hairpin_max, double homodimer_max) {
+  	std::vector<primer> result;
 
     for (int i = 0; i < evaluated_primers.data.size(); i++) {
-      	primer current_primer = evaluated_primers.get(i);
+      	primer current_primer = evaluated_primers[i];
 
 
     	if (current_primer.tm < tm_min) continue;
@@ -146,30 +186,41 @@ primer_group filter_primers(primer_group evaluated_primers, int tm_min, int tm_m
     	if (current_primer.hairpin > hairpin_max) continue;
     	if (current_primer.homodimer > homodimer_max) continue;
 
-		result.add(current_primer);
+		result.push_back(current_primer);
     }
 
 	return result;
 }
 
-primer_group rank_primers(primer_group data){
-	primer_group result;
+std::vector<primer> rank_primers(std::vector<primer> data, float target_tm, float target_gc){
 
+	for (int i = 0; i < data.size(); i++) {
 
-	return result;
+		float tm_score = abs(data[i].tm - target_tm);
+		float gc_Score = abs(data[i].gc - target_gc);
+		data[i].score = tm_score + gc_Score + data[i].hairpin + data[i].homodimer;
+		
+	}
+
+	// I think that this is sorted correctly...?
+
+	std::sort(data.begin(), data.end(), [](primer a, primer b) {
+		return a.score > b.score;
+	});
+	return data;
 }
 
-primer_group generate_matching_primers(primer_group data, primer_group allele_spesific_primers, int min_distance, int max_distance) {
+std::vector<primer> generate_matching_primers(std::vector<primer> data, std::vector<primer> allele_spesific_primers, int min_distance, int max_distance) {
 
-	primer_group result;
+	std::vector<primer> result;
 
-	for (int i = 0; i < allele_spesific_primers.data.size(); i++) {
+	for (int i = 0; i < allele_spesific_primers.size(); i++) {
 		
-		int snp_id = allele_spesific_primers.get(i).snp_id;
-		std::string allele = allele_spesific_primers.get(i).allele;
-		std::string direction = allele_spesific_primers.get(i).direction;
-		std::string sequence = allele_spesific_primers.get(i).primer_sequence;
-		int center = allele_spesific_primers.get(i).center;
+		int snp_id = allele_spesific_primers[i].snp_id;
+		std::string allele = allele_spesific_primers[i].allele;
+		std::string direction = allele_spesific_primers[i].direction;
+		std::string sequence = allele_spesific_primers[i].primer_sequence;
+		int center = allele_spesific_primers[i].center;
 
 
 
@@ -180,7 +231,7 @@ primer_group generate_matching_primers(primer_group data, primer_group allele_sp
 				if (direction == "forward") {
 					int start = center + dist;
 					std::string intermediate = sequence.substr(start, start + length);
-					std::string primer_seq = reverse_compliment(intermediate);
+					std::string primer_seq = reverse_complement(intermediate);
 				}
 				else {
 					int start = center - dist - length;
@@ -211,39 +262,15 @@ primer_group generate_matching_primers(primer_group data, primer_group allele_sp
     return result;
 }
 
-primer_group check_multiplex_compatibility(primer_group data, double heterodimer_max){
-  primer_group result;
+std::vector<primer> check_multiplex_compatibility(std::vector<primer> data, double heterodimer_max){
+  std::vector<primer> result;
 
 
 
   return result;
 }
 
-std::string reverse_complement(std::string s) {
-  
-  std::unordered_map<char, char> complement = {
-      
-    {'A', 'T'}, {'T', 'A'},
-    {'C', 'G'}, {'G', 'C'},
-    {'a', 't'}, {'t', 'a'},
-    {'c', 'g'}, {'g', 'c'}
-    
-    };
-  
-    std::string result;
-    
-    for(int i = 0; i < s.length(); i++) {
-      
-      char cur_complement = complement[s[i]];
-      result += cur_complement;
-      
-    }
-    
-    
-    return result;
-    
-  
-}
+
 
 
 
@@ -265,13 +292,6 @@ PYBIND11_MODULE(pcr_lib, m) {
         .def_readwrite("length", &primer::length)
         .def_readwrite("gc_content", &primer::gc_content)
         .def_readwrite("hairpin", &primer::hairpin)
-        .def_readwrite("homodimer", &primer::homodimer)
-    	.def("__getitem__", &primer::get);
-
-    py::class_<primer_group>(m, "primer_group")
-		.def(py::init<>())
-    	.def_readwrite("data", &primer_group::data)
-    	.def("__getitem__", &primer_group::get);
-
+        .def_readwrite("homodimer", &primer::homodimer);
 
 }
